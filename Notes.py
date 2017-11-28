301d2b7b7c7b890e5c0a1d283edbcb1d741a2009
DenimTX <31050506+DenimTX@users.noreply.github.com>
2017-11-28 15:40:53 -0800

    Added functions

--- Notes.py
+++ Notes.py
@@ -1,26 +1,52 @@
-print("Hello World")
+# print("Hello World")
 
 # Denim Xiong
+#
+# print()  # Creates a blank line
+# print("See if you can figure this out")
+# print(10 % 3)
 
-print()  # Creates a blank line
-print("See if you can figure this out")
-print(10 % 3)
-
-# Variables
-car_name = "Lamborguieapig"
-car_type = "Beast"
-car_cylinders = 8
-car_mpg = 9000.1
-
-# Inline Printing
-print("My car is the %s." % car_name)
-print("My car is the %s. It is a %s" % (car_name, car_type))
+# # Variables
+# car_name = "Lamborguieapig"
+# car_type = "Beast"
+# car_cylinders = 8
+# car_mpg = 9000.1
+#
+# # Inline Printing
+# print("My car is the %s." % car_name)
+# print("My car is the %s. It is a %s" % (car_name, car_type))
 # Taking input
-name = input("What is your name? ")
-print("Hello %s." % name)
-# print(name)
+# name = input("What is your name? ")
+# print("Hello %s." % name)
+# # print(name)
+#
+# age = input("How old are you? ")
+# print("Oh, so you're %s years old." % age)
+# # print(age)
 
-age = input("How old are you? ")
-print("Oh, so you're %s years old." % age)
-# print(age)
+# Change to the file
 
+
+def print_hw():
+    print("Hello World")
+
+
+print_hw()
+
+
+def say_hi(name):  # name is a "parameter
+    print("Hello %s." % name)
+    print("I hope you have a fantastic day.")
+
+
+say_hi("John")
+
+
+def birthday(age):
+    age += 1  # age = age + 1
+    print(age)
+
+
+say_hi("John")
+print("John is 15. Next year:")
+birthday(15)
