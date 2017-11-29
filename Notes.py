0260fc36beb5759569f499a424fe27729f5b9788
DenimTX <31050506+DenimTX@users.noreply.github.com>
2017-11-29 15:41:42 -0800

    Added more notes

--- Notes.py
+++ Notes.py
@@ -1,58 +1,108 @@
-# print("Hello World")
+# # print("Hello World")
+# #
 
-# Denim Xiong
-#
-# print()  # Creates a blank line
-# print("See if you can figure this out")
-# print(10 % 3)
 
-# # Variables
-# car_name = "Lamborguieapig"
-# car_type = "Beast"
-# car_cylinders = 8
-# car_mpg = 9000.1
+
+
+# # print()  # Creates a blank line
+# # print("See if you can figure this out")
+# # print(10 % 3)
 #
-# # Inline Printing
-# print("My car is the %s." % car_name)
-# print("My car is the %s. It is a %s" % (car_name, car_type))
-# Taking input
-# name = input("What is your name? ")
-# print("Hello %s." % name)
-# # print(name)
+# # # Variables
+# # car_name = "Lamborguieapig"
+# # car_type = "Beast"
+# # car_cylinders = 8
+# # car_mpg = 9000.1
+# #
+# # # Inline Printing
+# # print("My car is the %s." % car_name)
+# # print("My car is the %s. It is a %s" % (car_name, car_type))
+# # Taking input
+# # name = input("What is your name? ")
+# # print("Hello %s." % name)
+# # # print(name)
+# #
+# # age = input("How old are you? ")
+# # print("Oh, so you're %s years old." % age)
+# # # print(age)
 #
+# # Change to the file
+#
+#
+# <<<<<<< HEAD
+# def print_hw():
+#     print("Hello World")
+#
+#
+# print_hw()
+#
+#
+# def say_hi(name):  # name is a "parameter
+#     print("Hello %s." % name)
+#     print("I hope you have a fantastic day.")
+#
+#
+# say_hi("John")
+#
+#
+# def birthday(age):
+#     age += 1  # age = age + 1
+#     print(age)
+#
+#
+# say_hi("John")
+# print("John is 15. Next year:")
+# birthday(15)
+# =======
 # age = input("How old are you? ")
 # print("Oh, so you're %s years old." % age)
 # # print(age)
-
-# Change to the file
-
-
-<<<<<<< HEAD
-def print_hw():
-    print("Hello World")
+# >>>>>>> b42cc26d56f37d36349f215bc316ca27baf0b393
+# Press Ctrl-A and Ctrl-/
+# to comment old code
 
 
-print_hw()
+# def f(x):
+#     return x**5 + 4 * x **4 - 17*x**2 + 4
+#
+#
+# print(f(3))
+# print(f(3) + f(5))
+#
+# # If statements
+#
+#
+# def grade_calc(percentage):
+#     if percentage >= 90:
+#         return "A"
+#     elif percentage >= 80:  # Else if
+#         return "B"
+#     elif percentage >= 70:
+#         return "C"
+#     elif percentage >= 60:
+#         return "D"
+#     else:
+#         return "F"
+
+# Loops
 
 
-def say_hi(name):  # name is a "parameter
-    print("Hello %s." % name)
-    print("I hope you have a fantastic day.")
+# for num in range(5):
+#     print(num + 1)
 
 
-say_hi("John")
+a = 1
+while a < 10:
+    print(a)
+    a += 1
 
 
-def birthday(age):
-    age += 1  # age = age + 1
-    print(age)
+response = ""
+# while response != "Hello":
+#     response = input("Say \"Hello\"")
+
+print("Hello \nWorld")  # \n means newline
 
 
-say_hi("John")
-print("John is 15. Next year:")
-birthday(15)
-=======
-age = input("How old are you? ")
-print("Oh, so you're %s years old." % age)
-# print(age)
->>>>>>> b42cc26d56f37d36349f215bc316ca27baf0b393
+import random  # imports should be at the top
+print(random.randint(0, 6))
