bf2717d272af9302ee68fb3bfd880bda19d80fe6
DenimTX <31050506+DenimTX@users.noreply.github.com>
2017-12-07 15:41:35 -0800

    12.7.17
    Finish!

--- Denim - Guessgame.py
+++ Denim - Guessgame.py
@@ -7,13 +7,20 @@ import random
 # 5) Add 5 guesses
 number = (random.randint(1, 50))
 # print(number)   to know what the number is
+print(number)
 print("I am thinking of a number between 1 and 50.")
 guess = 5
-while int(guess) != number:
+guesses_made = 0
+while int(guess) != number and guesses_made < 5:
     guess = input("What number am I thinking of?")
     if (int(guess)) == number:
         print("Correct")
     elif int(guess) > number:
-        print("Lower.")
+        print("Lower")
+        guesses_made += 1
     elif int(guess) < number:
         print("Higher")
+        guesses_made += 1
+if guesses_made >= 5:
+    print("\nNo More Tries T.T")
+    print("The number was %" % number)
--- Warmups.py
+++ Warmups.py
@@ -51,4 +51,14 @@ def add(num1, num2, num3):
 
 
 add(90, 900, 9000)
-add()
+
+
+def repeat(string):
+    print(string)
+    print(string)
+    print(string)
+# repeat("string")
+
+
+for x in range(3):
+    print("string")  # prints string three times
