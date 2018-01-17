ac89290959ea82f2e935aaa88bcf26c67b358275
DenimTX <31050506+DenimTX@users.noreply.github.com>
2018-01-17 15:39:49 -0800

    1.17.18
    Restarted Blackjack

--- BlackJack.py
+++ BlackJack.py
@@ -1,16 +1,3 @@
 import random
 
-first = random.randint(1, 10)
-second = random.randint(1, 10)
-draw = random.randint(1, 10)
-hand = 0
-new = hand + draw
-start = hand + first + second
-print("Your hand:", start)
-
-while hand < 22:
-
-    answer = input("Do you want another card?")
-
-if answer == "yes":
-    print(new)
+draw = random.randint(1, 11)
