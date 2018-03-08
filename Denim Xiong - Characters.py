43497555e14ee9982ab0e063a8a5476e745b14fd
DenimTX <31050506+DenimTX@users.noreply.github.com>
2018-03-08 15:39:05 -0800

    3.8.18
    .

--- Denim Xiong - Characters.py
+++ Denim Xiong - Characters.py
@@ -1,18 +1,37 @@
+import random
+
+
 class Character(object):
-    def __init__(self, name, inventory, health, stats, description):
+    def __init__(self, name, inventory, health, description, attack):
         self.name = name
         self.inventory = inventory
         self.health = health
-        self.stats = stats
         self.description = description
-
-zeus = Character("Zeus", [], 100, [], 'An Old And Useless Man')
-zeus.take_damage
+        self.attack = attack
+        self.death = False
 
     def take_damage(self, amount):
+        self.health -= amount
+
+    def swing(self, enemy):
+        enemy.take_damage(self.attack)
+        print("You attacked %s." % enemy.name)
+        if enemy.health <= 0:
+            enemy.death = True
+            print("%s died." % enemy.name)
+
+    def fight(self, enemy):
+        print("You start a fight with $s" % enemy.name)
+
+        while self.health != 0:
+            choice = random.choice([enemy, self])
+            if choice == self:
+                enemy.swing(self)
+            elif choice == enemy:
+                self.swing(enemy)
 
 
-    def fight(self, target):
+you = Character('Zeus', [], 100, 'A useless old man.', random.randint(1, 10))
+enemy = Character('Baron Nashor', [], 100, 'A purple and sturdy worm.', random.randint(1, 20))
 
-
-    def death(self, health):
+you.fight(enemy)
