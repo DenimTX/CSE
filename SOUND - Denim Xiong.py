8ed0e18cf24265a46c309397bbe72266eb422876
Denim Xiong 5493 <yft8@fresnou.org>
2018-05-17 15:40:25 -0700

    5.17.18
    ..

--- SOUND - Denim Xiong.py
+++ SOUND - Denim Xiong.py
@@ -3,10 +3,14 @@ import winsound
 notes = {'C': 0, 'D': 2, 'E': 4, 'F': 5, 'G': 7, 'A': 9, 'B': 11}
 
 
-def play_note(note, duration=500):
+def play_note(note, duration=250):
     winsound.Beep(int(256 * (2 ** (notes[note] / 12))), duration)
 
 
-song = "E D E D E G D C A C E A B E A B C E D E D E B D C A C E A B E B C A B C D E G F E D"
+song = "E D C D E E E D D D C C C"
 for note in song.split():
+    play_note(note)
+
+song1 = "E D C D E E E E D D E D C"
+for note in song1.split():
     play_note(note)
